e29acd73ed4bb84603e9d16c4c944b2b064559da
GeorgyPasyuk <115622303+GeorgyPasyuk@users.noreply.github.com>
2024-01-22 12:07:57 +0300

    cloud fix

--- src/callback-query/cloud/cloudQuery.ts
+++ src/callback-query/cloud/cloudQuery.ts
@@ -2,7 +2,6 @@
 import {bot} from "@index/*";
 import {cloudR} from "@shared/regexp/cloudRegexp";
 import {cloudText} from "@shared/text/cloud/cloud";
-import {forumId} from "@shared/forum/forumId";
 export const cloudQuery = () => {
     bot.on("callback_query", async (ctx) => {
         try {
@@ -10,16 +9,8 @@ export const cloudQuery = () => {
                 await bot.sendMessage(
                     ctx.message.chat.id,
                     cloudText
-                ).then(() => bot.addListener("photo", async (photo)=> {}))
-                bot.on('photo', async ()=> {
-
-                    await bot.createForumTopic(forumId, `${ctx.message.chat.first_name} ${ctx.message.chat.id.toString()}`)
-                    await bot.sendMessage(
-                        ctx.message.chat.id,
-                        'Спасибо! Теперь пожалуйста отправьте данное фото в WhatsApp по номеру представленному ниже:'
-                    ).then(()=> bot.removeAllListeners('photo'))
-                    await bot.sendContact(ctx.message.chat.id, "+79858153238", 'Поддержка REGGO')
-                })
+                )
+                await bot.sendContact(ctx.message.chat.id, "+79858153238", 'Поддержка REGGO')
             }
         } catch (error) {
             console.log(error);
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import TelegramBot from "node-telegram-bot-api";
 import {menuCommands} from "@shared/commands";
 import {connectCommands} from "@commands/botCommands";
 import {connectCallbackQueries} from "./callback-query";
-import {forumId} from "@shared/forum/forumId";
+
 
 require('dotenv').config();
 
@@ -10,17 +10,6 @@ const { TG_TOKEN: token } = process.env;
 
 export const bot = new TelegramBot(token, { polling: true });
 bot.setMyCommands(menuCommands!);
-
-bot.on('text', async (msg)=> {
-    if (msg.chat.id === forumId) {
-        const userId = msg.reply_to_message.forum_topic_created.name.split(' ')[1]
-        await bot.sendMessage(userId, msg.text)
-    } else {
-        //await bot.sendMessage(forumId, msg.text, {message_thread_id: 82})
-        // const threadId = ''
-        // await bot.sendMessage(forumId, msg.text, {message_thread_id: threadId})
-    }
-})
 
 
 connectCommands();
