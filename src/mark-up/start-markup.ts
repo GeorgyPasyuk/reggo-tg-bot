03a83d0d7655300462f4a89480e10a2a50d52f5e
Georgy <pasyukgeorgy@gmail.com>
2024-03-22 11:13:55 +0300

    cloud text update

--- src/mark-up/start-markup.ts
+++ src/mark-up/start-markup.ts
@@ -3,7 +3,7 @@ export const startMarkup = {
     reply_markup: {
         inline_keyboard: [
             [{ text: "Проблема с камерой", callback_data: "camera-issue" }],
-            [{ text: "Облачное хранилище", callback_data: "cloud-storage" }],
+            [{ text: "Получите подарок", callback_data: "cloud-storage" }],
             [{ text: "Часто задаваемые вопросы", callback_data: "faq" }],
             [{ text: "Минимальные требования", callback_data: "requirements" }],
         ],
