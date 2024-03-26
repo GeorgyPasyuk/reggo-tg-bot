ce6fa1496c7fdb66e35fe97b6dc4a26753d4325b
Georgy <pasyukgeorgy@gmail.com>
2024-03-26 14:07:46 +0300

    button camera-issue rename

--- src/mark-up/start-markup.ts
+++ src/mark-up/start-markup.ts
@@ -2,7 +2,7 @@
 export const startMarkup = {
     reply_markup: {
         inline_keyboard: [
-            [{ text: "Проблема с камерой", callback_data: "camera-issue" }],
+            [{ text: "Видеоинструкция/Проблема с камерой", callback_data: "camera-issue" }],
             [{ text: "Получите подарок", callback_data: "cloud-storage" }],
             [{ text: "Часто задаваемые вопросы", callback_data: "faq" }],
             [{ text: "Минимальные требования", callback_data: "requirements" }],
